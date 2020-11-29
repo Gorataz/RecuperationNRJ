cf695e202a40cd78c7fb0c94b5f862ec4c6e1b07
Gorataz <jerem.aub@protonmail.com>
2020-11-29 19:20:26 +0100

    Modification des programmes RX, TX ; succès de l'envoie/réception d'une valeur de température en float

--- Programme_Base_RX/Programme_Base_RX.ino
+++ Programme_Base_RX/Programme_Base_RX.ino
@@ -1,140 +1,73 @@
 /*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
+
+Radio    Arduino
+CE    -> 9
+CSN   -> 10 (Hardware SPI SS)
+MOSI  -> 11 (Hardware SPI MOSI)
+MISO  -> 12 (Hardware SPI MISO)
+SCK   -> 13 (Hardware SPI SCK)
+IRQ   -> Rien
+VCC   -> En dessous de 3.6 volts
+GND   -> GND
+
+Servo  Arduino
+GND -> GND (noir ou marron)
+5V -> 5V (rouge)hufhuveFQbuhovefhbuFZQbiufq
+Signal -> 6 (orange ou blanc)
+
 */
-
 #include <SPI.h>
-#include "RF24.h"
+#include <NRFLite.h>
 
-/****************** User Config ***************************/
-/***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7& & 8 */
-RF24 radio(7,8);
-/**********************************************************/
 
-byte addresses[][6] = {"1Node","2Node"};
+const static uint8_t RADIO_ID = 0;       // Numéro d'indentification de notre module récepteur
+const static uint8_t PIN_RADIO_CE = 9; //déclaration du pin CE
+const static uint8_t PIN_RADIO_CSN =10;//déclaration du pin CSN
 
-// Used to control whether this node is sending or receiving
-bool role = 0;
+struct RadioPacket // Packet de données envoyé,
+//uint8_t = variable sur 8 bit, uint16_t=variable sur 16 bit, uint32_t=variable sur 32 bit
+{
+    uint32_t Valeur;//variable Valeur codée sur 32 bit
+};
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println(F("RF24/examples/GettingStarted"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
-  
-  radio.begin();
+NRFLite _radio; //Déclaration du module NRF
+RadioPacket _radioData;//Déclaration du packet de données
 
-  // Set the PA Level low to prevent power supply related issues since this is a
- // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_LOW);
-  
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-  }
-  
-  // Start the radio listening for data
-  radio.startListening();
+
+void setup()
+{
+    Serial.begin(115200);//On ouvre la laison série avec le PC à 115200 baud
+    int Valeur;
+
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))//si l'initialisation du module n'est pas finie
+    {
+        Serial.println("L'arduino ne peut pas communiquer avec le module ");
+        while (1); // Attend
+    }
+    Serial.println("Setup fini");
+    
+    /*
+    Par défaut la vitesse est de 2mb/s sur la channel 100/125
+    L'émetteur et le recepteur doivent etre sur la même channel et avoir la même vitesse pour bien communiquer
+    
+    Il est possible de changer ces paramtres avec les commandes:    
+    
+    _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)//250kB/s channel 0
+    _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)//1 MB/s channel 75
+    _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // par défaut
+    */
 }
 
-void loop() {
-  
-  
-/****************** Ping Out Role ***************************/  
-if (role == 1)  {
-    
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
-    Serial.println(F("Now sending"));
-
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &start_time, sizeof(unsigned long) )){
-       Serial.println(F("failed"));
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
+void loop()
+{
+    float Valeur;
+    while (_radio.hasData())//Tant que le module NRF a des données
+    {
+        _radio.readData(&_radioData); // lecture des nouvelles données 
+        Valeur=float(_radioData.Valeur)/100;
+        Serial.print("Température : ");
+        Serial.print(Valeur);//afficher sur lepc la valeur recue
+        Serial.println("°C");
     }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-
-
-/****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
-  {
-    unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-
-
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
-
-
-} // Loop
+}
--- Programme_Base_TX/Programme_Base_TX.ino
+++ Programme_Base_TX/Programme_Base_TX.ino
@@ -1,140 +1,69 @@
 /*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
+
+Radio    Arduino
+CE    -> 9
+CSN   -> 10 (Hardware SPI SS)
+MOSI  -> 11 (Hardware SPI MOSI)
+MISO  -> 12 (Hardware SPI MISO)
+SCK   -> 13 (Hardware SPI SCK)
+IRQ   -> Rien
+VCC   -> En dessous de 3.6 volts
+GND   -> GND
+
+
+Potentiomètre Arduino
+Pin de droite -> 5V
+Pin du millieu -> A0
+Pin de gauche -> GND
 */
 
+
 #include <SPI.h>
-#include "RF24.h"
+#include <NRFLite.h>
 
-/****************** User Config ***************************/
-/***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
+const static uint8_t RADIO_ID = 1;             // Numéro d'indentification de notre module émetteur
+const static uint8_t DESTINATION_RADIO_ID = 0; // Numéro d'identification du module récepteur (A qui notre émetteur transmet)
+const static uint8_t PIN_RADIO_CE = 9;//déclaration du pin CE
+const static uint8_t PIN_RADIO_CSN = 10;//déclaration du pin CSN
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
-/**********************************************************/
+int Valeur;//variable qui contient la valeur transmise
 
-byte addresses[][6] = {"1Node","2Node"};
+struct RadioPacket // Packet de données envoyé,
+//uint8_t=variable sur 8 bit, uint32_t=variable sur 32 bit
+{
+    uint32_t Valeur;//variable Valeur codée sur 32 bit
+};
 
-// Used to control whether this node is sending or receiving
-bool role = 1;
+NRFLite _radio; //Déclaration du module NRF
+RadioPacket _radioData;//Déclaration du packet de données
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println(F("RF24/examples/GettingStarted"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
-  
-  radio.begin();
+void setup()
+{
+    Serial.begin(115200);//On ouvre la laison série avec le PC à 115200 baud
 
-  // Set the PA Level low to prevent power supply related issues since this is a
- // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_LOW);
-  
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-  }
-  
-  // Start the radio listening for data
-  radio.startListening();
+       
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))//si l'initialisation du module n'est pas finie
+    {
+        Serial.println("L'arduino ne peut pas communiquer avec le module ");
+        while (1); // Attend
+    }
+    Serial.println("Setup fini");
+    
 }
 
-void loop() {
-  
-  
-/****************** Ping Out Role ***************************/  
-if (role == 1)  {
+void loop()
+{
+    _radioData.Valeur=2256;
+    Serial.println(_radioData.Valeur);//Affiche la valeur envoyée
+    _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData),NRFLite::NO_ACK); //Si le module récepteur confirme qu'il a bien recu le message
+    delay(3000);//pause de 3 secondes
     
-    radio.stopListening();                                    // First, stop listening so we can talk.
+    /*
+    Par défaut la commande _radio.send transmet les données puis attend pour un accusé de réception
     
+    Il est possible de changer cette condition avec ces commandes :
     
-    Serial.println(F("Now sending"));
-
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &start_time, sizeof(unsigned long) )){
-       Serial.println(F("failed"));
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-
-
-/****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
-  {
-    unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-
-
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
-
-
-} // Loop
+    _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::NO_ACK)//pas d'accusé de réception
+    _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::REQUIRE_ACK) // par défaut , accusé de réception
+    */
+}
